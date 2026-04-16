18bd856f60d908d1a382ffce6d4645da9cd9eaf9
john <john.f.roufaeil@gmail.com>
2026-04-16 20:57:51 +0200

    feat: create latest & featured projects apis, refactor: move top_rated to projects

--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -3,6 +3,6 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register('', views.ProjectViewSet, basename='project')
+router.register('', views.ProjectViewSet, basename='projects')
 
 urlpatterns = [path('', include(router.urls))]
--- apps/ratings/urls.py
+++ apps/ratings/urls.py
@@ -4,7 +4,6 @@ from .views import (
     RatingUpdateView,
     ProjectRatingsListView,
     ProjectRatingSummaryView,
-    TopRatedProjectsView,
     RecentRatingsView,
 )
 urlpatterns = [
@@ -12,6 +11,5 @@ urlpatterns = [
     path('projects/<int:project_id>/ratings/update/', RatingUpdateView.as_view(), name='rating-update'),
     path('projects/<int:project_id>/ratings/list/', ProjectRatingsListView.as_view(), name='project-ratings'),
     path('projects/<int:project_id>/ratings/summary/', ProjectRatingSummaryView.as_view(), name='project-rating-summary'),
-    path('top-rated/', TopRatedProjectsView.as_view(), name='top-rated-projects'),
     path('recent-rated/', RecentRatingsView.as_view(), name='recent-ratings'),
 ]
