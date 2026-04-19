92d8dba0d21e9634cbc14c8e2ac1208e95889490
menna7634 <mennamohammed178@gmail.com>
2026-04-19 02:02:07 +0200

    fix prod.py for production 2

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,27 +4,34 @@ from decouple import config
 import dj_database_url
 
 
+# =========================
 # SECURITY
+# =========================
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 DEBUG = False
 
 ALLOWED_HOSTS = [
     ".railway.app",
     ".up.railway.app",
+    "localhost",
 ]
 
 
+# =========================
 # DATABASE (Supabase)
+# =========================
 DATABASES = {
     'default': dj_database_url.parse(
-        config('DATABASE_URL'),
+        os.environ["DATABASE_URL"],
         conn_max_age=600,
         ssl_require=True
     )
 }
 
 
-# CORS & CSRF
+# =========================
+# CORS / CSRF
+# =========================
 FRONTEND_URL = os.environ.get(
     "FRONTEND_URL",
     "https://fundegypt.vercel.app"
@@ -41,23 +48,27 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 
-# EMAIL 
+# =========================
+# EMAIL (Gmail SMTP)
+# =========================
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
 EMAIL_HOST_USER = os.environ['EMAIL_USER']
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASS']
-
 DEFAULT_FROM_EMAIL = f'FundEgypt <{EMAIL_HOST_USER}>'
 
 
-#  HTTPS SECURITY
+# =========================
+# SECURITY HEADERS
+# =========================
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
-SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 
@@ -67,6 +78,8 @@ X_FRAME_OPTIONS = 'DENY'
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
-#  STATIC FILES
+# =========================
+# STATIC FILES
+# =========================
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
