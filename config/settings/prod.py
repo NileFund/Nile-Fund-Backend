b72ff52e633543538a83c84bcc3f9c69a7137cb3
menna7634 <mennamohammed178@gmail.com>
2026-04-19 03:59:06 +0200

    fix email section in production last time i hope

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -52,12 +52,12 @@ CSRF_TRUSTED_ORIGINS = [
 # EMAIL (Gmail SMTP)
 # =========================
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 587
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp-relay.brevo.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-EMAIL_HOST_USER = os.environ['EMAIL_USER']
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASS']
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 DEFAULT_FROM_EMAIL = f'FundEgypt <{EMAIL_HOST_USER}>'
 
 
